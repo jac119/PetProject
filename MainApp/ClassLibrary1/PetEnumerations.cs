3d581f8432ccce267183f932aaba3d7324dace21
Jeffrey <thekidwiththemullett@hotmail.com>
2015-03-23 21:53:10 -0400

    stuff?

---
+++ MainApp/ClassLibrary1/PetEnumerations.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseAccessAndConstraints
+{
+    class PetEnumerations
+    {   //Enum for colors, that way we don't allow too complex of a description
+        private enum Colors
+        {
+            Black,
+            Brown,
+            White,
+            Gold,
+            Red
+        }
+        //enum for patterns, again keeping descriptions less complex
+        private enum Pattern
+        {
+            Spots,
+            Stripes,
+            Solid
+        }
+        //TODO: add lengths descriptions so users have solid base to agree on
+        private enum Lengths
+        {
+            Small,
+            Medium,
+            Large
+        }
+        //TODO enums probably should be public, unless we grab them in this class to fill boxes
+    }
+}
